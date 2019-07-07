1fdd3b5af149d8a84aaf10edc71c4cdb3f9f0e02
Ricardo Salcido <ricardo.salcido@outlook.com>
2019-07-07 19:18:46 +0000

    lesson 21

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :models
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
+  resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
